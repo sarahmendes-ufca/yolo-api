c685f2f48a5062e66a84c73a475b8092af3b61ee
Sarah <sarah.mendes@aluno.ufca.edu.br>
2026-08-28 14:35:05 -0300

    Fix: Fix main imports

--- app/main.py
+++ app/main.py
@@ -6,9 +6,8 @@ import time
 import httpx
 import numpy as np
 from fastapi import FastAPI, HTTPException, Response
-from PIL import Image
-
 from model import get_default_model_name, load_model
+from PIL import Image
 from schemas import (
     BatchPredictRequest,
     BatchPredictResponse,
